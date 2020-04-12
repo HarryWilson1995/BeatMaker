878b88af439a63e025314fa9ad0e37d3972e6ed6
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-04-12 16:01:20 -0400

    adding sounds to each pad and fixing current time issue

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ class Drumkit {
     this.hihatAudio = document.querySelector(".hihat-sound");
     this.index = 0;
     this.bpm = 150;
+    this.isPlaying = null;
   }
   activePad() {
     this.classList.toggle("active");
@@ -14,11 +15,32 @@ class Drumkit {
   repeat() {
     let step = this.index % 8;
     const activeBars = document.querySelectorAll(`.b${step}`);
+    // Loop over pads
+    activeBars.forEach((bar) => {
+      bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+      // Check if pad is active
+      if (bar.classList.contains("active")) {
+        // Check each sound
+        if (bar.classList.contains("kick-pad")) {
+          this.kickAudio.currentTime = 0;
+          this.kickAudio.play();
+        }
+        if (bar.classList.contains("snare-pad")) {
+          this.snareAudio.currentTime = 0;
+          this.snareAudio.play();
+        }
+        if (bar.classList.contains("hihat-pad")) {
+          this.hihatAudio.currentTime = 0;
+          this.hihatAudio.play();
+        }
+      }
+    });
     this.index++;
   }
   start() {
     const interval = (60 / this.bpm) * 1000;
-    setInterval(() => {
+    // Check if it's playing
+    this.isPlaying = setInterval(() => {
       this.repeat();
     }, interval);
   }
@@ -28,6 +50,9 @@ const drumkit = new Drumkit();
 
 drumkit.pads.forEach((pad) => {
   pad.addEventListener("click", drumkit.activePad);
+  pad.addEventListener("animationend", function () {
+    this.style.animation = "";
+  });
 });
 
 drumkit.playBtn.addEventListener("click", () => {
